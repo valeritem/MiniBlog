cfc95b0ab6656a9d1005d3b82451fd39e6cbd322
Lera <valeri2006tem@gmail.com>
2025-11-05 15:39:54 +0200

    add user schema

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unigue: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        posts: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+        },
+    ],
+    }
+    ,
+    {timestamps: true},
+)
+
+export default mongoose.model('User', UserSchema)
