9c9f384fb3689518a7540fcdf21462772b1905de
Lera <valeri2006tem@gmail.com>
2025-12-10 22:59:32 +0200

    style: fix code formatting

--- client/src/pages/PostsPage.jsx
+++ client/src/pages/PostsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { PostItem } from '../components/PostItem';
 import axios from '../utils/axios';
 
